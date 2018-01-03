d1d2a14f1a95d419e4ca50a52c4670e6d6d031f1
ExtraConcentratedJuice <onomatopoeicboom@gmail.com>
2018-01-02 17:58:22 -0800

    first commit

---
+++ CommandClearItems.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+using Rocket.API;
+using Rocket.Unturned.Chat;
+using SDG.Unturned;
+
+namespace ExtraConcentratedJuice.ExtraItemClear
+{
+    public class CommandClearItems : IRocketCommand
+    {
+        #region Properties
+
+        public AllowedCaller AllowedCaller => AllowedCaller.Both;
+
+        public string Name => "clearitems";
+
+        public string Help => "Clears all of the items in the world.";
+
+        public string Syntax => "/clearitems";
+
+        public List<string> Aliases => new List<string>();
+
+        public List<string> Permissions => new List<string> { "extraitemclear.clearitems" };
+
+        #endregion
+
+        public void Execute(IRocketPlayer caller, string[] args)
+        {
+            ItemManager.askClearAllItems();
+            UnturnedChat.Say(caller, "Items cleared successfully!");
+        }
+    }
+}
---
+++ ExtraItemClear.cs
@@ -0,0 +1,13 @@
+﻿using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+
+namespace ExtraConcentratedJuice.ExtraItemClear
+{
+    public class ExtraItemClear : RocketPlugin
+    {
+        protected override void Load()
+        {
+            Logger.Log("Extra's Item Clear Loaded!");
+        }
+    }
+}
